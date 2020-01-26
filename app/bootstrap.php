653900caf189cf47728a1fb817c451e1366f5654
David Pustai <david@pustai.cz>
2020-01-26 12:26:36 +0100

    Use only one local config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,10 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('888.888.888.888'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
-//$configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(realpath(__DIR__ . '/../temp'));
 
 $configurator->createRobotLoader()
@@ -15,12 +13,7 @@ $configurator->createRobotLoader()
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
-
-/** Load config according to host name */
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) || !in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
-	$configurator->addConfig(__DIR__ . '/config/config.product.neon');
-else
-	$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 $container = $configurator->createContainer();
 
